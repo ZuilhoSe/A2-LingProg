d05303ba25f09eec5771facb7b23c8fb914e7e21
BrunoKauanL <brunokauanlunardon@gmail.com>
2022-11-29 13:34:25 -0300

    Add the main menu and pause to the game

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import pygame, sys
 from settings import *
 from level import Level
+from menus import Menus
+
 
 class Game:
 	def __init__(self):
@@ -10,13 +12,32 @@ class Game:
 		pygame.display.set_caption('Joguinho de testes que não está pronto')
 		self.clock = pygame.time.Clock()
 		self.level = Level()
-	
+		self.is_paused=False
+
+	def menu(self):
+		self.menu=Menus()
+		if self.menu.main_menu() == "play":
+			self.run()
+
 	def run(self):
 		while True:
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					pygame.quit()
 					sys.exit()
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_RETURN:
+						self.is_paused = True
+						self.pause=Menus()
+						self.pause.pause_menu()	
+			while self.is_paused:
+				for event in pygame.event.get():
+					if event.type == pygame.QUIT:
+						pygame.quit()
+						sys.exit()
+					if event.type == pygame.KEYDOWN:
+						if event.key == pygame.K_RETURN:
+							self.is_paused = False
 
 			self.screen.fill('black')
 			self.level.run()
@@ -25,4 +46,4 @@ class Game:
 
 if __name__ == '__main__':
 	game = Game()
-	game.run()	
+	game.menu()
