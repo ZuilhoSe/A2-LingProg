105e1fe8e9b59ae58fe7da644beaffbfefbc262e
Zuilho Segundo <segundozuilho@gmail.com>
2022-12-05 09:25:13 -0300

    change path

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ import pathlib
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-sys.path.insert(0, os.path.abspath('../../../src'))
+sys.path.insert(0, os.path.abspath('../src'))
 sys.path.insert(0, os.path.abspath('../source'))
 sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
 
